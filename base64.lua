7a926cb6dab8a9d41a376d4648ae944e823c26c1
sven mattsen <sven@sven.cc>
2009-11-17 23:31:23 +0100

    simplified and made memoizetable recoverable

--- base64.lua
+++ base64.lua
@@ -1,45 +1,9 @@
-function f_b64(prod)
+function f_b64(prod, b64memoize)
+	if type(prod) ~= "thread" or b64memoize ~= false and type(b64memoize) ~= "table" then error("Wrong arg types to f_b64", 2) end
 	local base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	local function recv()
-		local status, s = coroutine.resume(prod)
-		return s
-	end
-	local send = coroutine.yield
-	return coroutine.create(function ()
-		local s = ""
-		while coroutine.status(prod) ~= "dead" do
-			local padd = 0
-			repeat
-				s = s .. recv()
-				if coroutine.status(prod) == "dead" then
-					if s ~= "" then padd = 3 - (s:len() % 3) end
-					s = s .. string.rep("\000", padd)
-					break
-				end
-			until s:len() >= 3
-			local ret = {}
-			local mod = s:len() % 3
-			for i = 1, s:len() - mod, 3 do
-				local bits = s:byte(i) * 2^16 + s:byte(i + 1) * 2^8 + s:byte(i + 2)
-				local s64 = ""
-				for n = 1, 4 do
-					local index = bits % 64 + 1
-					if n > padd then s64 = base64:sub(index, index) .. s64 end
-					bits = math.floor(bits / 64)
-				end
-				table.insert(ret, s64)
-			end
-			if mod == 0 then s = "" else s = s:sub(-mod) end
-			if padd ~= 0 then table.insert(ret, string.rep("=", padd)) end
-			send(table.concat(ret))
-		end
-	end)
-end
-
-function f_b64_meta(prod)
-	local base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	local base64memoize = {}
-	setmetatable(base64memoize, { __index = function (t, s3)
+	if b64memoize ~= false then setmetatable(b64memoize, { __mode = "v" }) end
+	local function tob64(s3)
+		if b64memoize == false or b64memoize[s3] == nil then
 			local bits = s3:byte(1) * 2^16 + s3:byte(2) * 2^8 + s3:byte(3)
 			local s64 = ""
 			for n = 1, 4 do
@@ -47,10 +11,12 @@ function f_b64_meta(prod)
 				s64 = base64:sub(index, index) .. s64
 				bits = math.floor(bits / 64)
 			end
-			t[s3] = s64
+			if b64memoize ~= false then b64memoize[s3] = s64 end
 			return s64
-	end })
-						
+		else
+			return b64memoize[s3]
+		end
+	end
 	local function recv()
 		local status, s = coroutine.resume(prod)
 		return s
@@ -67,12 +33,12 @@ function f_b64_meta(prod)
 			local mod = s:len() % 3
 			for i = 1, s:len() - mod, 3 do
 				local sub3 = s:sub(i, i + 3)
-				table.insert(ret, base64memoize[sub3])
+				table.insert(ret, tob64(sub3))
 			end
 			if coroutine.status(prod) == "dead" and mod > 0 then
 				local subs = s:sub(s:len() - mod, -1)
 				local padd = 3 - subs:len()
-				subs = base64memoize[subs .. ("\000"):rep(padd)]
+				subs = tob64(subs .. ("\000"):rep(padd))
 				table.insert(ret, subs:sub(1, 4 - padd) .. ("="):rep(padd))
 			end
 			if mod == 0 then s = "" else s = s:sub(-mod) end
@@ -118,5 +84,6 @@ function consumer(prod)
 	end
 end
 
-consumer(f_b64_meta(producer_file(...)))
---consumer(f_b64(producer_file(...)))
+local t = {}
+consumer(f_b64(producer_file(...), t))
+consumer(f_b64(producer_file(...), t))
